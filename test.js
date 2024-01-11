95a03163eb181043c4b786f99c3db1f02f91e1a5
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-11 15:30:55 +0100

    feat: install ESLint plugin

--- test.js
+++
@@ -1,7 +0,0 @@
-console.log(true || true); // true
-console.log(false || false); // false
-console.log(true || false); // true
-
-console.log(true && true); // true
-console.log(false && false); // false
-console.log(true && false); // false
