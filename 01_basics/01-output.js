deba3f700e1343cbf9a5d8d83eaca7bacfd9dd65
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-11 15:14:16 +0100

    feat: add `01-output` topic

---
+++ 01_basics/01-output.js
@@ -0,0 +1,37 @@
+console.log('JavaScript Output + html file');
+
+// console.log();
+console.log('Hello World!'); // Hello World!
+console.log(4 + 6); // 10
+console.log('First Name', 'Age:', 45); // First Name Age: 45
+console.log('First Name' + 'Age:' + 45); // First Name Age: 45
+
+console.log('Hello! \nGood How are you today?'); // \n - from a new line
+
+console.log(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Nunc nec malesuada libero. Class aptent taciti sociosqu 
+ad litora torquent per conubia nostra, per inceptos himenaeos.`);
+
+console.log(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc nec malesuada libero. Class aptent taciti sociosqu 
+            ad litora torquent per conubia nostra, per inceptos himenaeos.`);
+
+// innerHTML
+document.getElementById('id_name').innerHTML = 'Addition result: ' + (3 + 4); // Addition result: 7
+
+let elements = document.getElementsByClassName('class_name'); //Subtraction result: 6
+for (var i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = 'Subtraction result: ' + (10 - 4);
+}
+
+// write()
+document.write(8 + 9);
+
+// alert()
+alert('Be careful!');
+alert('Is it OK?');
+
+// warn()
+console.warn('Warning!');
+
+// https://www.w3schools.com/js/js_output.asp
