551036f6ce071a59a354298604b21f9f4d4576fb
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-15 18:12:04 +0100

    feat: add new task with variables

---
+++ exercises/001_exercise.js
@@ -0,0 +1,31 @@
+/*
+Task to do with variables.
+    1. create variables to describe the phone, remembering the
+       about camelCase so that the identifier names better
+       describe the contents of the variables. 
+    2. add a value to each variable: numeric or text
+*/
+
+let mobilePhone = 'Samsung';
+let phoneModel = 'Galaxy S24 Ultra';
+let yearOfManufacture = 2024;
+let color = 'Black';
+let price = '1449 euro';
+
+mobilePhone = 'iPhone';
+phoneModel = '15 Pro Max';
+yearOfManufacture = '2023';
+color = 'Blue titanium';
+price = '9 599 zł';
+
+console.log(
+  'Phone: ',
+  mobilePhone,
+  phoneModel,
+  ', ',
+  yearOfManufacture,
+  ', ',
+  color,
+  ', ',
+  price,
+);
