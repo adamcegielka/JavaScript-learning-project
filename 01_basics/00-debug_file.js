c96954b6333bba6dcdcb90c74c2e225f81c2d4f4
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-15 19:17:27 +0100

    feat: add file for code debug

---
+++ 01_basics/00-debug_file.js
@@ -0,0 +1,8 @@
+// click F5
+
+const firstName = 'Adam';
+const lastName = 'Brick';
+const age = 41;
+
+const message = `Hello, my name is ${firstName} ${lastName} I'm ${age}`
+console.log(message);
