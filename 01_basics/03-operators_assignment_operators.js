b0aee891fd8ba989b8960f9f3e5208c2a676c806
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-16 19:01:25 +0100

    feat: add new lesson with operators

---
+++ 01_basics/03-operators_assignment_operators.js
@@ -0,0 +1,32 @@
+console.log('JavaScript Operators');
+
+// Assignment Operators
+
+let a = 9;
+let b = 4;
+
+a += 8;
+b += 3;
+console.log(a, b); // 17, 7
+
+a -= 7;
+b -= 2;
+console.log(a, b); // 10, 5
+
+a *= 2;
+b *= 3;
+console.log(a, b); // 20, 15
+
+a /= 8;
+b /= 2;
+console.log(a, b); // 2.5, 7.5
+
+a **= 2;
+b **= 2;
+console.log(a, b); // 6.25, 56.25
+
+a %= 3;
+b %= 20;
+console.log(a, b); // 0.25, 16.25
+
+// https://www.w3schools.com/js/js_assignment.asp
