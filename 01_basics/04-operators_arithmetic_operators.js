2cfca148bec8925c4984a844dd5400550d412aa1
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-16 19:26:53 +0100

    feat: add new lesson with operators

---
+++ 01_basics/04-operators_arithmetic_operators.js
@@ -0,0 +1,57 @@
+console.log('JavaScript Operators');
+
+// Arithmetic Operators
+
+let a = 3;
+let b = 1;
+
+a = 12;
+b = 5;
+
+// Addition
+const result1 = a + b;
+console.log(result1); // 17
+
+// Subtraction
+const result2 = a - b;
+console.log(result2); // 7
+
+// Multiplication
+const result3 = a * b;
+console.log(result3); // 60
+
+// Exponentiation
+const result4 = a ** b; // 248832
+console.log(result4);
+
+// Division
+const result5 = a / b; // 2.4
+console.log(result5);
+
+// Modulus
+const result6 = a % b; // 2
+console.log(result6);
+
+/*
+// increment x++
+a = 5;
+a++;
+console.log(a); // 6
+
+// pre-increment ++x
+a = 4;
+++a;
+console.log(a); // 5
+
+// decrement x--
+b = 5;
+b--;
+console.log(b); // 4
+
+// pre-decrement --x
+b = 8;
+--b;
+console.log(b); // 7
+*/
+
+// https://www.w3schools.com/js/js_arithmetic.asp
