d49178ddd9086dcbd2505e3aaf87289963d6f392
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-16 18:56:39 +0100

    refactor: code refactoring

--- 01_basics/00-debug_file.js
+++ 01_basics/00-debug_file.js
@@ -4,5 +4,5 @@ const firstName = 'Adam';
 const lastName = 'Brick';
 const age = 41;
 
-const message = `Hello, my name is ${firstName} ${lastName} I'm ${age}`
+const message = `Hello, my name is ${firstName} ${lastName} I'm ${age}`;
 console.log(message);
