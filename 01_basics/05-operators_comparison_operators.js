1c99d8c0ddf5b22e2d2f3f40ca04471962c8cddc
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-16 19:54:02 +0100

    feat: add new lesson with operators

---
+++ 01_basics/05-operators_comparison_operators.js
@@ -0,0 +1,33 @@
+console.log('JavaScript Comparison Operators');
+
+const c = 8;
+const d = 5;
+const e = 'yes';
+
+// ==	equal to
+console.log(c == d); // false
+
+// ===	equal value and equal type
+console.log(c === d); // false
+console.log(c === e); // false
+
+// !=	not equal
+console.log(c != d); // true
+
+// !==	not equal value or not equal type
+console.log(c !== d); // true
+console.log(c !== e); // true
+
+// >	greater than
+console.log(c < d); // false
+
+// <=	less than or equal to
+console.log(c <= d); // false
+
+// <	less than
+console.log(c > d); // true
+
+// >=	greater than or equal to
+console.log(c >= d); // true
+
+// https://www.w3schools.com/js/js_comparisons.asp
