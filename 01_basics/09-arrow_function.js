b92a268dda79793d25e9a10e57458ca1dac63264
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-20 18:13:53 +0100

    feat: add new lesson with Arrow Functions

---
+++ 01_basics/09-arrow_function.js
@@ -0,0 +1,13 @@
+console.log('Arrow Function');
+
+const helloFunction = () => 'Hi, how are you?';
+
+helloFunction();
+
+// ----------------
+
+const arrowFunction = (x, y) => x * y;
+
+arrowFunction(2, 4);
+
+// https://www.w3schools.com/js/js_functions.asp
