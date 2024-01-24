083043b9fb28539a02f31e5bd8abd18077b09467
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-24 15:23:44 +0100

    feat(js): add three ways for function

--- 01_basics/08-functions.js
+++ 01_basics/08-functions.js
@@ -38,6 +38,22 @@ returnAdd(10, 2); // 12
 
 // ----------------
 
+function getDisplayName1(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+
+const getDisplayName2 = function getDisplayName2(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+};
+
+const getDisplayName3 = (firstName, lastName) => `${firstName} ${lastName}`;
+
+console.log(getDisplayName1('Adam', 'Kowalski'));
+console.log(getDisplayName2('Ewa', 'Nowak'));
+console.log(getDisplayName3('John', 'Smith'));
+
+// ----------------
+
 /*
 const variableFunction = function() {
     let subtraction = 8 - 3;
