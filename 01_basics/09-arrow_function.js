3f038adeec3aec42c2917a327f332b118686a0d7
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-25 12:30:04 +0100

    feat: add `console.log`

--- 01_basics/09-arrow_function.js
+++ 01_basics/09-arrow_function.js
@@ -4,10 +4,14 @@ const helloFunction = () => 'Hi, how are you?';
 
 helloFunction();
 
+console.log(helloFunction());
+
 // ----------------
 
 const arrowFunction = (x, y) => x * y;
 
 arrowFunction(2, 4);
+
+console.log(arrowFunction(2, 4));
 
 // https://www.w3schools.com/js/js_functions.asp
