d4ab82606c0430940d0682eb85bc2ee357a433fc
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-25 11:56:50 +0100

    chore: edit content

--- 01_basics/00-debug_file.js
+++ 01_basics/00-debug_file.js
@@ -2,7 +2,8 @@
 
 const firstName = 'Adam';
 const lastName = 'Brick';
-const age = 41;
+const age = 53;
+const task = 'this is my exercise';
 
-const message = `Hello, my name is ${firstName} ${lastName} I'm ${age}`;
+const message = `Hello, my name is ${firstName} ${lastName} I'm ${age} and ${task}`;
 console.log(message);
