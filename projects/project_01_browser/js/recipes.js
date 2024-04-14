92346b15708cf25e437b6ef7c1160b594f290f5b
Adam Cegiełka <adamcegielka@tlen.pl>
2024-04-14 16:56:02 +0200

    refactor: format code with prettier

--- projects/project_01_browser/js/recipes.js
+++ projects/project_01_browser/js/recipes.js
@@ -1,18 +1,19 @@
 let recipes = {};
 
 const toggleRecipe = (e) => {
-    const drinkName = e.target.textContent.trim();
-    const recipeDiv = e.currentTarget.querySelector('.recipe');
-  
-    if (!recipeDiv) {
-      const newRecipeDiv = document.createElement('div');
-      newRecipeDiv.className = 'recipe';
-      newRecipeDiv.textContent = recipes[drinkName] || 'Recipe not available..';
-      e.currentTarget.appendChild(newRecipeDiv);
-    } else {
-      recipeDiv.style.display = recipeDiv.style.display === 'none' ? 'block' : 'none';
-    }
-  };
+  const drinkName = e.target.textContent.trim();
+  const recipeDiv = e.currentTarget.querySelector('.recipe');
+
+  if (!recipeDiv) {
+    const newRecipeDiv = document.createElement('div');
+    newRecipeDiv.className = 'recipe';
+    newRecipeDiv.textContent = recipes[drinkName] || 'Recipe not available..';
+    e.currentTarget.appendChild(newRecipeDiv);
+  } else {
+    recipeDiv.style.display =
+      recipeDiv.style.display === 'none' ? 'block' : 'none';
+  }
+};
 
 const initializeApp = () => {
   const li = document.querySelectorAll('.drink-list ul li');
